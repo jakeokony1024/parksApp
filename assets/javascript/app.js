63c2a64036e8442cf6dea1d8bc26ea0ccaf7b8f3
Jakeokony1024 <jakeokony@gmail.com>
2021-10-09 13:36:06 -0400

    data from response formatted into card structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,50 @@
 $(document).ready(function() {
  $.ajax({
-
   type: 'GET',
-  url: 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=' + API_KEY
+  url: 'https://developer.nps.gov/api/v1/parks?limit=12&api_key=' + API_KEY
  }).then(function(response){
-  console.log(response)
+  const parkData = response.data;
+  //Getting all Data for each park into variables //
+  for (let i = 0; i < parkData.length; i++) {
+   let title       = parkData[i].fullName;
+   let description = parkData[i].description;
+   let parkLink    = parkData[i].url;
+   let latitude    = parkData[i].latitude;
+   let longitude   = parkData[i].longitude;
+   let activityList= parkData[i].activities; 
+
+   //Building out the Card//
+   let cardHTML = `
+   <div class="card">
+    <div class="card-contents">
+     <h2 class="card-title">${title}</h2>
+     <img class="card-image" src="">
+     <p class="card-description">${description}</p>
+     <ul class="card-activities-list></ul>
+     <span class="location-info">${latitude + longitude}</span>
+     <a class="park-link" href="${parkLink}"><span class="park-link-text">Visit ${title}'s Homepage</span></a>
+    </div>
+   </div>`;
+   console.log(cardHTML);
+   $('.cards-section').append(cardHTML);
+  };
+  // parkData.forEach(parkData => {
+  //  for (let key in parkData) {
+  //   // console.log(`${key}: ${parkData[key]}`);
+  //   // console.log(`${parkTitle}: ${parkData[parkTitle]}`);
+  //  }
+  // });
+
  });
 
 
 });
+
+
+{/* <div class="card">
+  <div class="card-contents">
+    <h2 class="card-title"></h2>
+    <img class="card-image" src="">
+    <p class="card-description"></p>
+  </div>
+</div> */}
