02258ea44be24a3c4e250ded68404ee22b9de141
Jakeokony1024 <jakeokony@gmail.com>
2021-10-09 14:40:14 -0400

    all html sections laid out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,38 @@
 $(document).ready(function() {
  $.ajax({
   type: 'GET',
-  url: 'https://developer.nps.gov/api/v1/parks?limit=12&api_key=' + API_KEY
+  url: 'https://developer.nps.gov/api/v1/parks?&api_key=' + API_KEY,
+  data: {
+   limit: '12',
+   stateCode: 'ca'
+  }
+
  }).then(function(response){
   const parkData = response.data;
+  console.log(parkData);
   //Getting all Data for each park into variables //
   for (let i = 0; i < parkData.length; i++) {
+   let parkImgSrc  = parkData[i].images[0].url;
    let title       = parkData[i].fullName;
    let description = parkData[i].description;
    let parkLink    = parkData[i].url;
-   let latitude    = parkData[i].latitude;
-   let longitude   = parkData[i].longitude;
+   let latitude    = Math.round(parkData[i].latitude * 1000) / 1000;
+   let longitude   = Math.round(parkData[i].longitude * 1000) / 1000;
    let activityList= parkData[i].activities; 
+
 
    //Building out the Card//
    let cardHTML = `
    <div class="card">
     <div class="card-contents">
      <h2 class="card-title">${title}</h2>
-     <img class="card-image" src="">
+     <img class="card-image" src="${parkImgSrc}">
      <p class="card-description">${description}</p>
-     <ul class="card-activities-list></ul>
-     <span class="location-info">${latitude + longitude}</span>
+     <span class="location-info">${latitude} N</span>
+     <span class="location-info">${longitude} W</span>
      <a class="park-link" href="${parkLink}"><span class="park-link-text">Visit ${title}'s Homepage</span></a>
     </div>
    </div>`;
-   console.log(cardHTML);
    $('.cards-section').append(cardHTML);
   };
   // parkData.forEach(parkData => {
